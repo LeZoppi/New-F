ac0aaba2cd5f5be94aaf4539a3658cdf344566a8
LeZoppi <zoppinicolas7@gmail.com>
2021-07-15 15:05:24 -0300

    first commit

---
+++ altajuego.js
@@ -0,0 +1,88 @@
+addEventListener("load", load);
+
+var servidor = "https://backendTienda.herokuapp.com/altajuego";
+
+
+function $(demo){
+    return document.getElementById(demo);
+}
+
+function load() {
+    document.getElementById("guardar").addEventListener("click", click);
+     
+}
+ function click(){
+    enviarMensajeAlServidorPost(servidor, retornoDelClick);
+ }
+ function retornoDelClick(respuesta){
+        alert(respuesta);  
+}
+ 
+function enviarMensajeAlServidorPost(servidor, funcionARealizar){
+ 
+     var xmlhttp = new XMLHttpRequest();
+     var obje = new FormData();
+     obje.append("titulo",$("titulo").value);
+     obje.append("anio",$("anio").value);
+     obje.append("duracion",$("duracion").value);
+     obje.append("puntaje",$("puntaje").value);
+     obje.append("imagen",$("imagen").value);
+     obje.append("trailer",$("trailer").value);
+     obje.append("descripcion",$("descripcion").value);
+    
+     var msg="falta ingresar datos en \n";
+     var ok=true;
+
+     if($('titulo').value==""){
+         msg+="Titulo\n";
+         ok=false;
+     }
+     if($('duracion').value==""){
+        msg+="Duracion\n";
+        ok=false;
+     }
+     if($('descripcion').value==""){
+        msg+="Descripcion\n";
+        ok=false;
+     }
+     if($('puntaje').value==""){
+        msg+="puntaje\n";
+        ok=false;
+     }
+     if($('imagen').value==""){
+        msg+="imagen\n";
+        ok=false;
+     }
+     if($('anio').value==""){
+        msg+="año\n";
+        ok=false;
+     }
+     if($('trailer').value==""){
+        msg+="Trailer\n";
+        ok=false;
+     }
+     if(ok==false){
+        alert(msg);
+
+     }
+     else{
+        xmlhttp.open("POST", servidor, true);
+        xmlhttp.onreadystatechange = function () {
+            //Veo si llego la respuesta del servidor
+                                        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                                            //Reviso si la respuesta es correcta
+                                            if (xmlhttp.status == 200) {
+                                                funcionARealizar(xmlhttp.responseText);
+                                            }
+                                            else {
+                                                alert("ocurrio un error");
+                                            }
+                                        }
+                                     }
+        xmlhttp.setRequestHeader("enctype", "multipart/form-data");
+        
+        //envio el mensaje    
+        xmlhttp.send(obje);
+    }
+
+}
---
+++ juegos.js
@@ -0,0 +1,234 @@
+addEventListener("load", load);
+//llamo al servidor
+var servidor = "https://backendTienda.herokuapp.com/peliculas";
+var serviDelete = "https://backendTienda.herokuapp.com/eliminarpelicula";
+var serviFormMod = "https://backendTienda.herokuapp.com/FormModPelicula";
+var serviModJuego = "https://backendTienda.herokuapp.com/modificarpelicula";
+
+function $(demo){
+    return document.getElementById(demo);
+}
+
+
+
+function load(){
+    enviarMensajeAlServidor(servidor , cargarJuegos);
+
+}
+
+function cargarJuegos(valor) {
+
+    var juegos = JSON.parse(valor);
+    var todo=[];
+    juegos.forEach(element => {
+        todo.push('<div class="col-md-3"  style="padding:1%;">'+
+                      '<div class="card" style="width: 12.5rem;background:#212121;color:white">'+
+                        '<img src="'+element.imagen+'" class="card-img-top">'+
+                        '<p><i class="fas fa-star"></i>'+element.puntaje+'</p>'+
+                        '<div class="card-body" style="height:180px">'+
+                           '<p align="center" class="card-text">'+element.titulo+'</p>'+
+                           '<input class="form-control" type="text" id="id_juego" name="id_juego" value="'+element.id_juego+'" hidden>'+
+                           '<button type="submit" style="float: left;margin: 5px;border-radius:30px"  id="modificar" class="btn btn-dark" onclick="formMod('+element.id_juego+')"><i class="fas fa-pencil-alt"></i></a>'+
+                           '<button type="submit" style="text-decoration:underline;cursor:pointer; float: left;margin-right:5px;border-radius:30px;margin-top: 2%" class="btn btn-light card-text" onclick="eliminarDato('+element.id_juego+')"><i class="fas fa-trash-alt"></i></button>'+
+                        '</div>'+
+                      '</div>'+
+                  '</div>'
+
+                 );
+                 
+ 
+    });
+    $('juegos').innerHTML=todo;
+    
+    
+}
+function eliminarDato(id){
+    var eliminar = confirm('De verdad desea eliminar este dato?');
+
+
+    if ( eliminar ) {
+          
+        var xmlhttp = new XMLHttpRequest();
+        var datos = new FormData();
+        datos.append("id_juego",id);
+    
+        // indico hacia donde va el mensaje
+        xmlhttp.open("POST", serviDelete, true);
+        //seteo el evento
+        xmlhttp.onreadystatechange = function () {
+            //Veo si llego la respuesta del servidor
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                //Reviso si la respuesta es correcta
+                if (xmlhttp.status == 200) {
+                    alert("eliminado");
+                }
+                else {
+                    alert("ocurrio un error");
+                }
+            }
+        
+        }
+    
+       
+        xmlhttp.setRequestHeader("enctype", "multipart/form-data");
+    
+        //envio el mensaje    
+        xmlhttp.send(datos);
+        }
+        
+}
+function formMod(id){
+    enviarMensajeAlServidorPost(serviFormMod,retornoDelClick);
+
+
+
+    function retornoDelClick(respuesta){
+        $('juegos').hidden=true;
+      
+        var juegos = JSON.parse(respuesta);
+        var todo=[];
+        juegos.forEach(element => {
+        todo.push('<div class="col-md-12">'+
+                        '<div class="form-row">'+
+                            '<div class="form-group col-md-8">'+
+                                '<label for="inputEmail4">Titulo</label>'+
+                                '<input type="text" class="form-control" name="titulo" id="titulo" value="'+element.titulo+'">'+
+                             '</div>'+
+                             '<div class="form-group col-md-4">'+
+                                 '<label for="inputPassword4">año</label>'+
+                                 '<input type="text" class="form-control" name="anio" id="anio" value="'+element.anio+'">'+
+                             '</div>'+
+                        '</div>'+
+                        '<div class="form-row">'+
+                            '<div class="form-group col-md-8">'+
+                                '<label for="inputPassword4">Duracion</label>'+
+                                '<input type="text" class="form-control" name="duracion" id="duracion" value="'+element.duracion+'">'+
+                            '</div>'+
+                            '<div class="form-group col-md-4">'+
+                                '<label for="inputPassword4">Puntaje</label>'+
+                                '<input type="text" class="form-control" name="puntaje" id="puntaje" value="'+element.puntaje+'">'+
+                            '</div>'+
+                        '</div>'+
+                        '<div class="form-group">'+
+                            '<label for="inputEmail4">imagen</label>'+
+                            '<input type="text" class="form-control" name="imagen" id="imagen" value="'+element.imagen+'">'+
+                        '</div>'+
+                        '<div class="form-group">'+
+                            '<label for="inputEmail4">trailer</label>'+
+                            '<input type="text" class="form-control" name="trailer" id="trailer" value="'+element.trailer+'">'+
+                        '</div>'+
+                        '<div class="form-group">'+
+                            '<label for="inputEmail4">Descripcion</label>'+
+                            '<textarea type="text" class="form-control" name="descripcion" id="descripcion" value="'+element.descripcion+'">'+element.descripcion+'</textarea>'+
+                        '</div>'+
+                        '<div class="form-group">'+
+                           '<button type="submit" name="modificar" id="modificar" class="btn btn-dark" onclick="modificarDato('+element.id_juego+','+$("titulo").value+','+$("anio").value+','+$("duracion").value+','+$("puntaje").value+','+$("imagen").value+','+$("trailer").value+','+$("descripcion").value+')">Modificar</button>'+
+                        '</div>'+
+                '</div>'
+                 );
+                 
+ 
+        });
+    
+        $('form').innerHTML=todo;
+    }
+
+    function enviarMensajeAlServidorPost(serviFormMod, funcionARealizar){
+        var xmlhttp = new XMLHttpRequest();
+        var datos = new FormData();
+        datos.append("id_juego",id);
+    
+        // indico hacia donde va el mensaje
+
+        xmlhttp.open("POST", serviFormMod, true);
+        //seteo el evento
+        xmlhttp.onreadystatechange = function () {
+            //Veo si llego la respuesta del servidor
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                //Reviso si la respuesta es correcta
+                if (xmlhttp.status == 200) {
+                    funcionARealizar(xmlhttp.responseText);
+                }
+                else {
+                    alert("ocurrio un error");
+                }
+            }
+        
+        }
+    
+       
+        xmlhttp.setRequestHeader("enctype", "multipart/form-data");
+    
+        //envio el mensaje    
+        xmlhttp.send(datos);
+     }
+        
+}
+function modificarDato(id,titulo,anio,duracion,puntaje,imagen,trailer,descripcion){
+    enviarMensajeAlServidorPost(serviModJuego,retornoDelClick);
+
+
+
+    function retornoDelClick(respuesta){
+        alert(respuesta);
+    }
+
+    function enviarMensajeAlServidorPost(serviModJuego, funcionARealizar){
+        var xmlhttp = new XMLHttpRequest();
+        var datos = new FormData();
+        datos.append("id_juego",id);
+        datos.append("titulo",titulo);
+        datos.append("anio",anio);
+        datos.append("duracion",duracion);
+        datos.append("puntaje",puntaje);
+        datos.append("imagen",imagen);
+        datos.append("trailer",trailer);
+        datos.append("descripcion",descripcion);
+    
+        // indico hacia donde va el mensaje
+        xmlhttp.open("POST", serviModJuego, true);
+        //seteo el evento
+        xmlhttp.onreadystatechange = function () {
+            //Veo si llego la respuesta del servidor
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                //Reviso si la respuesta es correcta
+                if (xmlhttp.status == 200) {
+                    funcionARealizar(xmlhttp.responseText);
+                }
+                else {
+                    alert("ocurrio un error");
+                }
+            }
+        
+        }
+    
+       
+        xmlhttp.setRequestHeader("enctype", "multipart/form-data");
+    
+        //envio el mensaje    
+        xmlhttp.send(datos);
+     }
+        
+}
+
+
+function enviarMensajeAlServidor(servidor, funcionARealizar){
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open("GET",servidor,true);
+   
+    xmlhttp.onreadystatechange = function(){
+
+        if(xmlhttp.readyState == XMLHttpRequest.DONE){
+            if(xmlhttp.status == 200){
+                console.log(xmlhttp.response);
+                funcionARealizar(xmlhttp.responseText);
+            }else{
+                alert("Ocurrio un error");
+            }
+        }
+
+    }
+    xmlhttp.send();    
+}
+
